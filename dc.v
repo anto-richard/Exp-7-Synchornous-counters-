0c4c8f30434b7df2704c4a453856c72b1741e75f
Anto Richard. S <93427534+anto-richard@users.noreply.github.com>
2023-06-07 14:25:42 +0530

    push

---
+++ dc.v
@@ -0,0 +1,11 @@
+module dc(clk,A);
+input clk;
+output reg [3:0]A;
+always @(posedge clk)
+begin
+A[3]=((~A[2])&(~A[1])&(~A[0]))^A[3];
+A[2]=((~A[1])&(~A[0]))^A[2];
+A[1]=(~A[0])^A[1];
+A[0]=1^A[0];
+end
+endmodule
